8fabb54a91a9fe42cade28ea8b3c16302a4cda07
cgolian <Morrissey45>
2020-05-16 12:39:03 +0200

    Add Challenge 8.

--- src/set1/challenge7.ts
+++ src/set1/challenge7.ts
@@ -1,7 +1,7 @@
 import {BitArray, Uint8BitArray} from "./challenge1";
 import {XORBitArrays} from "./challenge2";
 
-const AES_128_BLOCK_LENGTH_BYTES = 16;
+export const AES_128_BLOCK_LENGTH_BYTES = 16;
 const AES_128_NUMBER_OF_ROUNDS = 10;
 
 const sBoxTable = [
---
+++ src/set1/challenge8.ts
@@ -0,0 +1,30 @@
+import {AES_128_BLOCK_LENGTH_BYTES} from "./challenge7";
+
+type BlockMap = {
+    [block: string]: number;
+}
+
+/**
+ * Detect ECB mode (always encrypting the same plaintext block as the same ciphertext block) in AES encrypted ciphertext
+ * @param aesEncryptedCiphertext
+ */
+export function isECBEncrypted(aesEncryptedCiphertext: string): {
+    result: boolean;
+    blocks?: BlockMap;
+} {
+    const blocksMap: BlockMap = {};
+    let result = false;
+    for (let byteIdx = 0; byteIdx < aesEncryptedCiphertext.length; byteIdx += AES_128_BLOCK_LENGTH_BYTES) {
+        const block = aesEncryptedCiphertext.slice(byteIdx, byteIdx + AES_128_BLOCK_LENGTH_BYTES);
+        if (blocksMap[block]) {
+            blocksMap[block] = blocksMap[block] + 1;
+            result = true;
+        } else {
+            blocksMap[block] = 1;
+        }
+    }
+    return {
+        result,
+        blocks: result ? blocksMap : undefined,
+    };
+}
