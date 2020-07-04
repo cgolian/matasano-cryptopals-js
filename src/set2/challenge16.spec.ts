ee3acb6b8436a06b5dd7aabd092ffa5a037eae6a
cgolian <chgolian93@gmail.com>
2020-07-04 19:15:31 +0200

    Add Challenge 16.

---
+++ src/set2/challenge16.spec.ts
@@ -0,0 +1,19 @@
+import {cbcFlipBits, encryptData, isAdmin} from "./challenge16";
+
+describe("Challenge 16", () => {
+    describe("CBC flip bits attack", () => {
+       let encryptionFn: (userInput: string) => Buffer;
+       let validationFn: (ciphertext: Buffer) => boolean;
+
+       beforeEach(() => {
+           encryptionFn = encryptData;
+           validationFn = isAdmin;
+       });
+
+       it('should validate modified ciphertext', () => {
+           const result = cbcFlipBits(encryptionFn, validationFn); // TEST
+
+           expect(result).toBeDefined();
+       });
+    });
+});
