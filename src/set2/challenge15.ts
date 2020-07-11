c81fd9e766d33f72614d454db439566eb1f27ad0
cgolian <chgolian93@gmail.com>
2020-07-11 08:38:42 +0200

    Add Challenge 17.

--- src/set2/challenge15.ts
+++ src/set2/challenge15.ts
@@ -5,7 +5,7 @@ export function unpadBlockPKCS7(plaintext: Buffer, blockLength: number): Buffer 
     if (plaintext.length % blockLength != 0) validationFailed = true;
     const lastBlock = plaintext.slice(plaintext.length - blockLength, plaintext.length);
     const lastByte = lastBlock[blockLength - 1];
-    if (lastByte > blockLength) validationFailed = true;
+    if (lastByte > blockLength || lastByte === 0x0) validationFailed = true;
     if (validationFailed || lastBlock.slice(lastBlock.length - lastByte, lastBlock.length).some(byte => byte != lastByte)) {
         throw Error(`Not valid PKCS7 padding`);
     }
