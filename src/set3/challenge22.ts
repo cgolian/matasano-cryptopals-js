7fdaecb23a775af3305abacbd268c8e6b4bb82a3
cgolian <chgolian93@gmail.com>
2020-07-20 10:55:34 +0200

    Finish Set 3.

---
+++ src/set3/challenge22.ts
@@ -0,0 +1,25 @@
+import {MT19937_BITS} from "./challenge21";
+
+async function sleep(millis: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+export async function randAfterRandomDelay(initRng: (seed: number, bits: MT19937_BITS) => () => number): Promise<{
+    generated: number;
+    seed: number;
+}> {
+    // Wait a random number of seconds between, I don't know, 40 and 1000.
+    let delay = Math.floor(Math.random() * (100 - 40 + 1)) + 40;
+    await sleep(delay * 1000);
+    // Seeds the RNG with the current Unix timestamp
+    const seed = Date.now();
+    const rng = initRng(seed, 32);
+    // Waits a random number of seconds again.
+    delay = Math.floor(Math.random() * (100 - 40 + 1)) + 40;
+    await sleep(delay * 1000);
+    // Returns the first 32 bit output of the RNG.
+    return {
+        generated: rng(),
+        seed: seed
+    };
+}
